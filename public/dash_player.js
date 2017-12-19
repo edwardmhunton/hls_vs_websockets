d03531d8e014f0a013ddfa2e45a6cd14fe96bb3a
edwardmhunton <edward.hunton@sky.uk>
2017-12-19 15:22:59 +0000

    tweaks

--- public/dash_player.js
+++ public/dash_player.js
@@ -1,6 +1,6 @@
 
 (function(){
-                var url = "./dash_content/test.mpd";
+                var url = "./dash_content/webcam.mpd";
                 var player = dashjs.MediaPlayer().create();
                 player.initialize(document.querySelector("#videoPlayer"), url, true);
             })();
